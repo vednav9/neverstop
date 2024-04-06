927f4ee331b1152dc7772217f4d3825618cf4ff4
vednav9 <vedantnavthale222@nhitm.ac.in>
2024-04-06 16:07:38 +0530

    a

--- event.php
+++ event.php
@@ -11,7 +11,7 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
       
-      <link rel="stylesheet" href="event.css">
+      <link rel="stylesheet" href="css/event.css">
 </head>
 
 <body>
